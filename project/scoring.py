e816f46695f8f19e69c627c95be44bc2c5467674
onsoim <onsoim@gmail.com>
2020-02-26 23:50:12 +0900

    scoring sheet

--- project/scoring.py
+++ project/scoring.py
@@ -7,10 +7,19 @@ def scoring(args):
     # answer = pd.read_excel(args.answer).drop_duplicates()
     correct = pd.read_csv(args.correct)
     answer = pd.read_csv(args.answer).drop_duplicates()
+    l_correct, l_answer = len(correct), len(answer)
 
-    wrong = pd.concat([correct, answer])
-    print(wrong.duplicated())
-    # right = 
+    score = pd.concat([correct, answer])
+    print(score.duplicated(keep=False)[:l_correct])
+
+    # # correct && answer
+    # true_positive = 
+
+    # # !correct && answer
+    # false_positive = 
+
+    # # correct && !answer
+    # false_negative = 
 
 
 def main():
